533ecd56ac4568acf9e85e334a5708b9d1177318
전지훈 <janus7359@gmail.com>
2023-02-28 17:58:04 +0900

    워드클라우드 긴급 수정
    d3기반으로 수정 예정

--- lib/chart/WordCloud.tsx
+++ lib/chart/WordCloud.tsx
@@ -1,9 +1,9 @@
-import React, { useCallback } from "react";
+import React from "react";
 import withParentSize from "../hooks/withParentSize";
-import { useEffect, useMemo } from "react";
-import { extent, scaleLog, select } from "d3";
+import { useMemo } from "react";
+import { extent, scaleLog } from "d3";
 import type { ComponentType } from "react";
-import cloud from "d3-cloud";
+import { Wordcloud } from "@visx/wordcloud";
 
 interface Props {
   /**
@@ -41,9 +41,9 @@ interface Props {
 
 const WordCloud: ComponentType<Props> = ({
   data,
-  id,
   width,
   height,
+  id,
   colorList = [
     "#0A0908",
     "#0891b2",
@@ -69,76 +69,116 @@ const WordCloud: ComponentType<Props> = ({
     return map;
   }, [colorList, data]);
 
-  const drawChart = useCallback(() => {
-    const cloud2 = cloud()
-      .size([width!, height!])
-      .words(
-        data.map((d) => {
-          return { text: d.text, size: d.value };
-        })
-      )
-      .rotate(0)
-      .spiral(type)
-      .random(() => 0.5)
-      .font("Impact")
-      .fontSize((d) => fontScale(d.size!) as any)
-      .on("end", draw);
+  // const drawChart = useCallback(() => {
+  //   const cloud2 = cloud()
+  //     .size([width!, height!])
+  //     .words(
+  //       data.map((d) => {
+  //         return { text: d.text, size: d.value };
+  //       }),
+  //     )
+  //     // .padding(4)
+  //     .rotate(0)
+  //     // .spiral('rectangular')
+  //     .random(() => 0.5)
+  //     .font('Impact')
+  //     .fontSize((d) => fontScale(d.size!) as any)
+  //     .on('end', draw);
+  //
+  //   cloud2.start();
+  //
+  //   function draw(words: any) {
+  //     select(`#${id}-svg`)
+  //       .attr(
+  //         'transform',
+  //         `translate(${cloud2.size()[0] / 2},${cloud2.size()[1] / 2})`,
+  //       )
+  //       .selectAll('text')
+  //       .data(words)
+  //       .join('text')
+  //       .style('font-size', function (d: any) {
+  //         return d.size + 'px';
+  //       })
+  //       .style('font-weight', 600)
+  //       .style('font-family', 'Impact')
+  //       .attr('text-anchor', 'middle')
+  //       .attr('transform', function (d: any) {
+  //         return `translate(${d.x}, ${d.y}) rotate(${d.rotate})`;
+  //       })
+  //       .style('fill', (d: any) => {
+  //         return colorMap.get(d.text);
+  //       })
+  //       .on('mouseover', function (e, d: any) {
+  //         const texts = (
+  //           select(select(e.target).node().parentNode).selectAll('text') as any
+  //         )._groups[0];
+  //         for (let text of texts) {
+  //           if (select(text).text() !== d.text) {
+  //             select(text).transition().attr('opacity', 0.1);
+  //           } else {
+  //             select(text).attr('opacity', 1);
+  //           }
+  //         }
+  //       })
+  //       .on('mouseleave', function (e, d: any) {
+  //         select(select(e.target).node().parentNode)
+  //           .selectAll('text')
+  //           .transition()
+  //           .attr('opacity', 1);
+  //       })
+  //       .text(function (d: any) {
+  //         return d.text;
+  //       });
+  //   }
+  //
+  //   return null;
+  // }, [colorMap, data, fontScale, height, id, setSelectedText, width]);
 
-    cloud2.start();
-
-    function draw(words: any) {
-      select(`#${id}-word-cloud`)
-        .attr(
-          "transform",
-          `translate(${cloud2.size()[0] / 2},${cloud2.size()[1] / 2})`
-        )
-        .selectAll("text")
-        .data(words)
-        .join("text")
-        .style("font-size", function (d: any) {
-          return d.size + "px";
-        })
-        .style("font-weight", 600)
-        .style("font-family", "Impact")
-        .attr("text-anchor", "middle")
-        .attr("transform", function (d: any) {
-          return `translate(${d.x}, ${d.y}) rotate(${d.rotate})`;
-        })
-        .style("fill", (d: any) => {
-          return colorMap.get(d.text);
-        })
-        .on("mouseover", function (e, d: any) {
-          const texts = (
-            select(select(e.target).node().parentNode).selectAll("text") as any
-          )._groups[0];
-          for (let text of texts) {
-            if (select(text).text() !== d.text) {
-              select(text).transition().attr("opacity", 0.1);
-            } else {
-              select(text).attr("opacity", 1);
-            }
-          }
-        })
-        .on("mouseleave", function (e, d: any) {
-          select(select(e.target).node().parentNode)
-            .selectAll("text")
-            .transition()
-            .attr("opacity", 1);
-        })
-        .text(function (d: any) {
-          return d.text;
-        });
-    }
-  }, [colorMap, data, fontScale, height, id, width, type]);
-
-  useEffect(() => {
-    drawChart();
-  }, [drawChart]);
+  // useEffect(() => {
+  //   drawChart();
+  // }, [drawChart]);
 
   return (
     <div className="h-full w-full select-text">
       <svg width={width} height={height}>
-        <g id={`${id}-word-cloud`} />
+        {/*<g id={`${id}-svg`} />*/}
+        <Wordcloud
+          words={data}
+          height={height!}
+          width={width!}
+          font={"Impact"}
+          fontSize={(datum) => fontScale(datum.value)}
+          random={() => 0.5}
+          // padding={2}
+          spiral={"archimedean"}
+          rotate={0}
+        >
+          {(cloudWords) =>
+            cloudWords.map((w) => {
+              return (
+                <text
+                  key={w.text}
+                  style={{ fontSize: w.size }}
+                  transform={`translate(${w.x}, ${w.y}) rotate(${w.rotate})`}
+                  textAnchor={"middle"}
+                  fill={colorMap.get(w.text)}
+                >
+                  {w.text}
+                </text>
+                // <Text
+                //   key={w.text}
+                //   fill={'black'}
+                //   textAnchor={'middle'}
+                //   transform={`translate(${w.x}, ${w.y}) rotate(${w.rotate})`}
+                //   fontSize={w.size}
+                //   fontFamily={w.font}
+                // >
+                //   {w.text}
+                // </Text>
+              );
+            })
+          }
+        </Wordcloud>
       </svg>
     </div>
   );
