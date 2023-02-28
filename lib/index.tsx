88d853d694a609e592e0017df73a2cb8aed5a986
Sowoon <janus7359@gmail.com>
2023-02-28 18:59:12 +0900

    wordcloud export 추가

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,3 +1,4 @@
 export { default as LineChart } from "./chart/LineChart";
 export { default as BarChart } from "./chart/BarChart";
 export { default as NetworkChart } from "./chart/NetworkChart";
+export { default as WordCloudChart } from "./chart/WordCloud";
