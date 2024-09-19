721a82121d385e4130bb5cbf006acae895c9f29b
sowoon <sowoon1221@gmail.com>
2024-09-19 16:12:48 +0900

    fix: 버젼수정 및 그룹바차트 export

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
-import LineChart from "./components/chart/LineChart.tsx";
-import BarChart from "./components/chart/BarChart.tsx";
-import Wordcloud from "./components/chart/Wordcloud.tsx";
-import NetworkChart from "./components/chart/NetworkChart.tsx";
-import PieChart from "./components/chart/PieChart.tsx";
-import BubbleChart from "./components/chart/BubbleChart.tsx";
-import RadarChart from "./components/chart/RadarChart.tsx";
-import StackLineChart from "./components/chart/StackLineChart.tsx";
-import AreaChart from "./components/chart/AreaChart.tsx";
-import StackBarChart from "./components/chart/StackBarChart.tsx";
-import ScatterChart from "./components/chart/ScatterChart.tsx";
-import GroupLineChart from "./components/chart/GroupLineChart.tsx";
+import LineChart from './components/chart/LineChart.tsx'
+import BarChart from './components/chart/BarChart.tsx'
+import Wordcloud from './components/chart/Wordcloud.tsx'
+import NetworkChart from './components/chart/NetworkChart.tsx'
+import PieChart from './components/chart/PieChart.tsx'
+import BubbleChart from './components/chart/BubbleChart.tsx'
+import RadarChart from './components/chart/RadarChart.tsx'
+import StackLineChart from './components/chart/StackLineChart.tsx'
+import AreaChart from './components/chart/AreaChart.tsx'
+import StackBarChart from './components/chart/StackBarChart.tsx'
+import ScatterChart from './components/chart/ScatterChart.tsx'
+import GroupLineChart from './components/chart/GroupLineChart.tsx'
+import GroupBarChart from './components/chart/GroupBarChart.tsx'
 
 export {
   LineChart,
@@ -24,4 +25,5 @@ export {
   AreaChart,
   ScatterChart,
   GroupLineChart,
-};
+  GroupBarChart
+}
