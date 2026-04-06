595e1b63b944efdd44ed22b34dbf1c166e1952f8
sowoon <sowoon1221@gmail.com>
2026-04-06 16:13:07 +0900

    refactor: 배럴 파일 export 구문 단순화

--- src/index.tsx
+++ src/index.tsx
@@ -1,58 +1,39 @@
-import AreaChart from './components/chart/AreaChart';
-import BarChart from './components/chart/BarChart';
-import BubbleChart from './components/chart/BubbleChart';
-import GroupBarChart from './components/chart/GroupBarChart';
-import GroupLineChart from './components/chart/GroupLineChart';
-import HeatmapChart from './components/chart/HeatmapChart';
-import LineChart from './components/chart/LineChart';
-import NetworkChart from './components/chart/NetworkChart';
-import PieChart from './components/chart/PieChart';
-import RadarChart from './components/chart/RadarChart';
-import ScatterChart from './components/chart/ScatterChart';
-import StackBarChart from './components/chart/StackBarChart';
-import StackLineChart from './components/chart/StackLineChart';
-import Wordcloud from './components/chart/Wordcloud';
-import ExportImage from './components/common/ExportImage';
-
-export {
-  AreaChart,
-  BarChart,
-  BubbleChart,
-  ExportImage,
-  GroupBarChart,
-  GroupLineChart,
-  HeatmapChart,
-  LineChart,
-  NetworkChart,
-  PieChart,
-  RadarChart,
-  ScatterChart,
-  StackBarChart,
-  StackLineChart,
-  Wordcloud,
-};
-
 export type { AreaChartProps } from './components/chart/AreaChart';
+export { default as AreaChart } from './components/chart/AreaChart';
 export type { BarChartProps } from './components/chart/BarChart';
+export { default as BarChart } from './components/chart/BarChart';
 export type { BubbleChartProps } from './components/chart/BubbleChart';
+export { default as BubbleChart } from './components/chart/BubbleChart';
 export type { GroupBarChartProps } from './components/chart/GroupBarChart';
+export { default as GroupBarChart } from './components/chart/GroupBarChart';
 export type { GroupedDatum } from './components/chart/GroupedChart.types';
 export type { GroupLineChartProps, GroupLineTooltipDatum } from './components/chart/GroupLineChart';
+export { default as GroupLineChart } from './components/chart/GroupLineChart';
 export type { HeatmapChartProps, HeatmapDatum } from './components/chart/HeatmapChart';
+export { default as HeatmapChart } from './components/chart/HeatmapChart';
 export type { LineChartProps } from './components/chart/LineChart';
+export { default as LineChart } from './components/chart/LineChart';
 export type {
   NetworkChartData,
   NetworkChartProps,
   NetworkLinkDatum,
   NetworkNodeDatum,
 } from './components/chart/NetworkChart';
+export { default as NetworkChart } from './components/chart/NetworkChart';
 export type { PieChartProps } from './components/chart/PieChart';
+export { default as PieChart } from './components/chart/PieChart';
 export type { RadarChartProps, RadarSeriesDatum } from './components/chart/RadarChart';
+export { default as RadarChart } from './components/chart/RadarChart';
 export type { ScatterChartProps, ScatterDatum } from './components/chart/ScatterChart';
+export { default as ScatterChart } from './components/chart/ScatterChart';
 export type { StackBarChartProps } from './components/chart/StackBarChart';
+export { default as StackBarChart } from './components/chart/StackBarChart';
 export type { StackLineChartProps } from './components/chart/StackLineChart';
+export { default as StackLineChart } from './components/chart/StackLineChart';
 export type { WordcloudProps } from './components/chart/Wordcloud';
+export { default as Wordcloud } from './components/chart/Wordcloud';
 export type { ExportImageProps } from './components/common/ExportImage';
+export { default as ExportImage } from './components/common/ExportImage';
 export type {
   BaseChartProps,
   CartesianChartProps,
